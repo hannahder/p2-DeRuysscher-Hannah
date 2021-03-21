a18037d2e80d294f9714cbe7cd0bb3279d6f9c31
Hannah De Ruysscher <hderuysscher@ufl.edu>
2021-03-21 09:45:00 -0400

    Added weather API, edited CSS

--- main.js
+++ main.js
@@ -61,3 +61,38 @@ function myFunction2() {
 for(i=0; i<colorChange2.length; i++) {
 colorChange2[i].addEventListener('click', myFunction2);
 };
+
+// National Weather API
+
+var weatherState = document.getElementById('state');
+
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        var apiResult = JSON.parse(this.responseText);
+
+        //Code Dealing With The API Data Goes Here
+        function showMessage() {
+        var message = document.createElement('h2');
+        weatherState.appendChild(message);
+        var text = document.createTextNode(apiResult.properties.periods[0].detailedForecast);
+        message.appendChild(text);
+        };
+        showMessage();
+    }
+};
+xmlhttp.open("GET", "https://api.weather.gov/gridpoints/MFL/109,98/forecast", true);
+xmlhttp.send();
+
+req.headers({
+	"x-rapidapi-key": "865836419bmshb3e9c373ff30073p15bae6jsnea5e6f9edc6c",
+	"x-rapidapi-host": "national-weather-service.p.rapidapi.com",
+	"useQueryString": true
+});
+
+
+req.end(function (res) {
+	if (res.error) throw new Error(res.error);
+
+	console.log(res.body);
+});
