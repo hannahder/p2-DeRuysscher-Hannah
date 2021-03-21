5fac03cd03d59b407f9fd3c378d22d97a6a1fb57
Hannah De Ruysscher <hderuysscher@ufl.edu>
2021-03-21 12:04:29 -0400

    Added JS messages to API

--- main.js
+++ main.js
@@ -73,26 +73,62 @@ xmlhttp.onreadystatechange = function() {
 
         //Code Dealing With The API Data Goes Here
         function showMessage() {
-        var message = document.createElement('h2');
+        var message = document.createElement('p');
         weatherState.appendChild(message);
-        var text = document.createTextNode(apiResult.properties.periods[0].detailedForecast);
+        var text = document.createTextNode(apiResult.properties.periods[0].temperature);
         message.appendChild(text);
         };
         showMessage();
+
+        let tooHot = () => {
+          let temperature = apiResult.properties.periods[0].temperature;
+          let hotTemp = document.getElementById("too-hot");
+          if (Number(temperature) > 90) {
+            hotTemp.classList.remove("hide");
+            } else {
+          }
+        }
+        setTimeout(tooHot, 1000);
+
+        let tooCold = () => {
+          let temperature = apiResult.properties.periods[0].temperature;
+          console.log(temperature);
+          let coldTemp = document.getElementById("too-cold");
+          if (Number(temperature) < 50) {
+            coldTemp.classList.remove("hide");
+            } else {
+          }
+        }
+        setTimeout(tooCold, 1000);
+
+
+        let justOptimal = () => {
+          let temperature = apiResult.properties.periods[0].temperature;
+          let optimalTemp = document.getElementById("optimal");
+          if (Number(temperature) >= 74 && Number(temperature) <= 86) {
+            optimalTemp.classList.remove("hide");
+            } else {
+          }
+        }
+        setTimeout(justOptimal, 1000);
     }
 };
 xmlhttp.open("GET", "https://api.weather.gov/gridpoints/MFL/109,98/forecast", true);
 xmlhttp.send();
 
-req.headers({
-	"x-rapidapi-key": "865836419bmshb3e9c373ff30073p15bae6jsnea5e6f9edc6c",
-	"x-rapidapi-host": "national-weather-service.p.rapidapi.com",
-	"useQueryString": true
-});
 
 
-req.end(function (res) {
-	if (res.error) throw new Error(res.error);
+// req.headers({
+// 	"x-rapidapi-key": "865836419bmshb3e9c373ff30073p15bae6jsnea5e6f9edc6c",
+// 	"x-rapidapi-host": "national-weather-service.p.rapidapi.com",
+// 	"useQueryString": true
+// });
+//
+//
+// req.end(function (res) {
+// 	if (res.error) throw new Error(res.error);
+//
+// 	console.log(res.body);
+// });
 
-	console.log(res.body);
-});
+//API temp for sea turtle hatchlings
